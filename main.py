5642ae79a5d0271ca55c92e3c2c56adb90d72dd2
y <y@yotouch.com>
2023-09-15 11:41:10 +0800

    add postbot

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+from datetime import datetime
+
 from fastapi import FastAPI
 import uvicorn
 
@@ -6,6 +9,31 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI!"}
+
+@app.post("/postbot/entry")
+async def postbot(request_body: dict):
+
+    dialog = request_body['dialog']
+    subjectId = dialog['subjectId']
+    sbId = dialog['sbId']
+    
+    q = dialog['question']
+    
+    
+    today = datetime.today()
+    formatted_date = today.strftime("%Y-%m-%d")
+    
+    content_list = [f"{formatted_date}\n{q}"]
+
+    response_body = {
+        "code": 200,
+        "msg": "OK",
+        "data": content_list,
+        "materialExtraInfoList": []
+    }
+    return response_body
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
